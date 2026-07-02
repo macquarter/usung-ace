5b8b4417c68a88f44e6b34df4092dde0e994522a
macquarter <41quart@gmail.com>
2026-07-03 00:02:22 +0900

    test: 고해상도 원본(KakaoTalk 1099x1000 / 1100x1500) 참조로 변경

--- usung-test-products.js
+++ usung-test-products.js
@@ -1,8 +1,8 @@
 (function(){
   'use strict';
-  // 실제 고해상도 제품 이미지 (repo products/ 폴더 내 파일 직접 참조)
-  var IMG_LED  = 'products/p102_LED450우주선동스텐파이프.jpg';        // 우주선 갓등 (동)
-  var IMG_STEN = 'products/p021_09_150Ø_스텐도금_반후지_텐션_크롬.jpg'; // 스텐 스윙텐션 (크롬)
+  // 유저가 업로드한 고해상도 원본 이미지 (products/ 폴더, 공백은 %20 인코딩)
+  var IMG_LED  = 'products/KakaoTalk_Photo_2026-07-01-16-45-42%20001.png'; // 우주선 갓등 (동) 1099x1000
+  var IMG_STEN = 'products/KakaoTalk_Photo_2026-07-01-16-45-42%20002.png'; // 스텐 스윙텐션 (크롬) 1100x1500
   var TEST = [
     { no:2001, img:IMG_LED,  name:'우주선 갓등 [테스트]',   cat:'2. LED 조명타입', sub:'우주선/갓등',    color:'동',  finish:'동',  pipe:'' },
     { no:2002, img:IMG_STEN, name:'스텐 스윙텐션 [테스트]', cat:'3. 스텐파이프',   sub:'도금/스윙 텐션', color:'크롬', finish:'크롬', pipe:'' }
@@ -31,7 +31,7 @@
       ACE.product_lineup.push({ no:t.no, img:t.img, name:t.name, cat:t.cat, sub:t.sub, color:t.color, finish:t.finish, pipe:t.pipe });
     });
     ACE._testInjected = true;
-    try { console.log('[usung-test v4] injected', TEST.length, 'products (real images)'); } catch(e){}
+    try { console.log('[usung-test v5] injected', TEST.length, 'products (hi-res originals)'); } catch(e){}
     var grid = document.getElementById('products-grid');
     if(grid && typeof window.filterByNode === 'function'){ window.filterByNode(''); }
     return true;
